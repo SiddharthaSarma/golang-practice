16a5ae76f6cd4875e373a7c76099edeeaaef130a
siddharthasarma <sidduinevitable@gmail.com>
2019-03-25 06:50:09 +0530

    variables in go

---
+++ go-with-examples/variables/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "siddhartha"
+	b := "Hello world"
+	c := 8.5555
+	d := 5
+
+	var e, f int = 8, 9
+	var g = true
+	fmt.Println(a, b)
+	fmt.Println(c, d, e, f)
+	fmt.Println(g)
+}
