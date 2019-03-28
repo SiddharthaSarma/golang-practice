0978c2965b67d40785f1ffa8ccf0296e9e478ad6
siddharthasarma <sidduinevitable@gmail.com>
2019-03-28 07:24:40 +0530

    splice

---
+++ go-by-examples/slices/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := make([]string, 3)
+	fmt.Println(c)
+
+	c[0] = "a"
+	c[1] = "b"
+	c[2] = "c"
+	fmt.Println("set:", c)
+	fmt.Println("get:", c[2])
+	// append
+	c = append(c, "e", "f", "g")
+	fmt.Println(c)
+	fmt.Println(len(c))
+
+	// copy
+	s := make([]string, len(c))
+	copy(s, c)
+	fmt.Println("cpy:", s)
+
+}
