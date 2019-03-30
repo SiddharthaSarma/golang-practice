33a2fcdad993684c0c06591180818795c734689e
siddharthasarma <sidduinevitable@gmail.com>
2019-03-30 10:37:07 +0530

    multiple return values

--- go-by-examples/functions/main.go
+++ go-by-examples/functions/main.go
@@ -9,8 +9,13 @@ func addThreeNumbers(a, b, c int) int {
 	return a + b + c
 }
 
+func vals() (int, int) {
+	return 2, 4
+}
+
 func main() {
 	fmt.Println(addTwoNumbers(2, 3))
 	sum := addThreeNumbers(2, 3, 4)
 	fmt.Println(sum)
+	fmt.Println(vals())
 }
