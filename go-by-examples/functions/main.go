722e375955a9e1327f444526f0e54f773ff7c7d7
siddharthasarma <sidduinevitable@gmail.com>
2019-03-31 14:35:27 +0530

    variadic functions

--- go-by-examples/functions/main.go
+++ go-by-examples/functions/main.go
@@ -13,9 +13,19 @@ func vals() (int, int) {
 	return 2, 4
 }
 
+func variadicFunc(nums ...int) int {
+	fmt.Println(nums)
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	return total
+}
+
 func main() {
 	fmt.Println(addTwoNumbers(2, 3))
 	sum := addThreeNumbers(2, 3, 4)
 	fmt.Println(sum)
 	fmt.Println(vals())
+	fmt.Println(variadicFunc(2, 3, 4))
 }
